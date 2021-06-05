932b3c9730c589a5c1600276442835929b13facb
nick <nick51365@gmail.com>
2021-06-05 19:49:14 -0400

    built static HTML page

---
+++ library.js
@@ -0,0 +1,14 @@
+let library = [
+    bookOne ={},
+];
+
+function Book (title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function(){
+        return title,"by",author,",",pages,"pages",",",read 
+    }
+}
+
