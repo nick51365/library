97e3ea716009e4785b4e8c6e8cbd0b7d318d7f2c
nick <nick51365@gmail.com>
2021-06-10 17:30:37 -0400

    added remove button functionality

--- library.js
+++ library.js
@@ -1,12 +1,16 @@
+//Unique entryID for each library entry
+let entryID = 0;
+
 //"library" array contains all submitted books
 let library = [];
 
 //Book constructor function
-function Book (title, author, pages, read){
+function Book (title, author, pages, read, entryID){
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+    this.entryID = entryID
 }
 
 //Loops through "library" array and displays contents in HTML table
@@ -14,7 +18,9 @@ function Book (title, author, pages, read){
 let table = document.getElementById("table");
 function displayBooks(library){
     for (i = 0; i < library.length; i++){
-        let row = table.insertRow(1);
+        let pos = library.length - 1;
+        let row = table.insertRow(i + 1);
+        row.setAttribute("data-id", entryID);
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
         let cell3 = row.insertCell(2);
@@ -25,13 +31,17 @@ function displayBooks(library){
         cell2.innerHTML = library[i].author;
         cell3.innerHTML = library[i].pages;
         cell4.innerHTML = library[i].read;
-        cell5.innerHTML = "<button></button";
+        cell5.innerHTML = `<button class="remove" onclick="removeRow(this)" id="btn${pos}"> </button>`;
+
+        entryID++;
     }
 }
+
 //Adds last "library" item to HTML table
 function addBook(library){
     let pos = library.length - 1;
-    let row = table.insertRow(1);
+    let row = table.insertRow(library.length);
+    row.setAttribute("data-id", entryID);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
@@ -42,8 +52,26 @@ function addBook(library){
     cell2.innerHTML = library[pos].author;
     cell3.innerHTML = library[pos].pages;
     cell4.innerHTML = library[pos].read;
-    cell5.innerHTML = "<button id=${`btn`+`pos`}> </button";
+    cell5.innerHTML = `<button class="remove" onclick="removeRow(this)" id="btn${pos}"> </button>`;
+    
+    entryID++;
 };
+
+//Removes selected row from HTML table
+function removeRow(delButton){
+    const rowIndex = delButton.parentNode.parentNode.rowIndex;
+    document.getElementById("table").deleteRow(rowIndex)
+    console.log(delButton.parentNode.parentNode.getAttribute("data-id"));
+
+//Finds corresponding Book object in library and deletes it
+    for (i = 0; i < library.length; i++){
+        if (library[i].entryID == delButton.parentNode.parentNode.getAttribute("data-id")){
+            library.splice(i, 1)
+            console.log(library);
+        }
+    }
+    console.log(library);
+}
 
 //Adds event listener to form
 const form = document.getElementById("form");
@@ -64,7 +92,8 @@ form.addEventListener("submit", function(event){
         document.getElementById("title").value, 
         document.getElementById("author").value, 
         document.getElementById("pages").value, 
-        read
+        read,
+        entryID,
         );
 
     //Add "book" object to "library" array
@@ -80,7 +109,35 @@ form.addEventListener("submit", function(event){
     document.getElementById("read").checked = false;
 });
 
-displayBooks(library);
+
+
+//Example books to prepopulate library for testing purposes
+library.push(new Book (
+    "First Textbook",
+    "Writer of Book",
+    123,
+    "Yes",
+    entryID,
+))
+addBook(library);
+
+library.push(new Book(
+    "Another Book",
+    "Smart Person",
+    456,
+    "No",
+    entryID,
+))
+addBook(library);
+
+library.push(new Book(
+    "One More Example",
+    "Idiot",
+    42069,
+    "No",
+    entryID,
+))
+addBook(library);
 
 
 
